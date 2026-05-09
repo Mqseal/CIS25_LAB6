21ef2b671ecb28fd3b4188f81b4ed3ec08139266
Mqseal <30034267@cc.peralta.edu>
2026-05-08 18:42:54 -0700

    Add Item class implementation

---
+++ Item.cpp
@@ -0,0 +1,30 @@
+#include "Item.h"
+#include <iostream>
+
+using namespace std;
+
+int Item::totalItems = 0;
+
+Item::Item(string name, int value)
+    : name(name), value(value) {
+}
+
+int Item::getTotalItems() {
+    return totalItems;
+}
+
+void Item::incrementTotalItems() {
+    totalItems++;
+}
+
+string Item::getName() const {
+    return name;
+}
+
+int Item::getValue() const {
+    return value;
+}
+
+void Item::display() const {
+    cout << "[" << name << "] (Value: " << value << ")" << endl;
+}
