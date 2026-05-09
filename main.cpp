ff844f650ff60eda8d11bd9c69f224ea7eaef438
Mqseal <30034267@cc.peralta.edu>
2026-05-08 18:43:47 -0700

    Lab 7: Added inventory and item system

--- main.cpp
+++ main.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include "Player.h"
+#include "Item.h"
 
 using namespace std;
 
 int main() {
     Player hero;
-    Player wizard("Gandalf", 120, 15);
+
+    Item sword("Sword", 10);
+    Item shield("Shield", 15);
+    Item potion("Potion", 5);
+    Item helmet("Helmet", 20);
+    Item boots("Boots", 8);
+    Item extra("Extra Item", 30);
 
     cout << endl;
-    cout << "--- Initial Status ---" << endl;
-    hero.displayStatus();
-    wizard.displayStatus();
+    cout << "--- Adding items to hero's inventory ---" << endl;
+
+    hero.addItem(sword);
+    hero.addItem(shield);
+    hero.addItem(potion);
+    hero.addItem(helmet);
+    hero.addItem(boots);
+    hero.addItem(extra);
 
     cout << endl;
-    cout << "--- Hero takes damage! ---" << endl;
-    hero.takeDamage(25);
-    hero.displayStatus();
+    cout << "--- Hero's Inventory ---" << endl;
+    hero.showInventory();
 
     cout << endl;
-    cout << "--- Wizard takes damage! ---" << endl;
-    wizard.takeDamage(150);
-    wizard.displayStatus();
+    cout << "Total items added to inventories: " << Item::getTotalItems() << endl;
 
     return 0;
 }
