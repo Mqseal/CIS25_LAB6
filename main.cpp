71fcf06381248b1897cff5248cab9f1a6df71d53
Mqseal <30034267@cc.peralta.edu>
2026-05-11 22:57:40 -0700

    Update main for game loop

--- main.cpp
+++ main.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
-#include "Player.h"
-#include "Item.h"
+#include "Game.h"
 
 using namespace std;
 
 int main() {
-    Player hero;
 
-    Item sword("Sword", 10);
-    Item shield("Shield", 15);
-    Item potion("Potion", 5);
-    Item helmet("Helmet", 20);
-    Item boots("Boots", 8);
-    Item extra("Extra Item", 30);
-
-    cout << endl;
-    cout << "--- Adding items to hero's inventory ---" << endl;
-
-    hero.addItem(sword);
-    hero.addItem(shield);
-    hero.addItem(potion);
-    hero.addItem(helmet);
-    hero.addItem(boots);
-    hero.addItem(extra);
-
-    cout << endl;
-    cout << "--- Hero's Inventory ---" << endl;
-    hero.showInventory();
-
-    cout << endl;
-    cout << "Total items added to inventories: " << Item::getTotalItems() << endl;
+    try {
+        Game game;
+        game.run();
+    }
+    catch (...) {
+        cout << "An error occurred." << endl;
+    }
 
     return 0;
 }
